6112b11e759841dc6a6a3d05f15a697c61822d8a
Mychal Hall <hallmychal@gmail.com>
2019-01-26 15:49:19 -0600

    functions complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -8,36 +8,44 @@
 */
 
 
-function consume (arg1,arg2,cb) {
-  return cb(arg1,arg2)
-}
-// function consume (arg1, arg2, cb) {
-//   return cb(arg1, arg2);
-  
+// function consume (arg1,arg2,cb) {
+//   return cb(arg1,arg2);
+
 // }
- 
+
+function consume (arg1, arg2, callBack) {
+   return callBack(arg1,arg2);
+}
+
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+  */
 
-function add(x , y) {
-  return cb (x + y)
+
+
+
+function add (arg1, arg2) {
+  return (arg1+arg2);
 }
 
-function multiply(x , y, cb) {
-  return cb (x * y)
+function multiply(arg1,arg2) {
+ return (arg1*arg2);
 }
 
-function greeting(firstName, lastName, cb) {
-  return cb (`Hello ${firstName}${lastName}, nice to meet you!`)
+function greeting(arg1, arg2) {
+  return (`Hello ${arg1} ${arg2}, nice to meet you!`)
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-consume(2,2,add); // 4
-consume(10,16,multiply); // 160
-consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+
+
 
 
 // ==== Closures ==== 
@@ -46,6 +54,7 @@ consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 // Explanation: 
 
+// Because they are both closed into the same function. Through closure the contained functions have access to the variables located inside the closure. I can draw it better than I can type it out.
 
 const external = "I'm outside the function";
 
